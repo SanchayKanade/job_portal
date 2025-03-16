5ce110c14f09673249092c914954cc022ba5bfa0
SanchayKanade <sanchay2598@gmail.com>
2025-03-16 11:21:40 -0500

    Job Controller Created

--- backend_server/backend_server/Core/AutoMapperConfig/AutoMapperConfigProfile.cs
+++ backend_server/backend_server/Core/AutoMapperConfig/AutoMapperConfigProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using backend_server.Core.Dtos.Company;
+using backend_server.Core.Dtos.Job;
 using backend_server.Core.Entities;
 
 namespace backend_server.Core.AutoMapperConfig
@@ -11,7 +12,11 @@ namespace backend_server.Core.AutoMapperConfig
             //Company
             CreateMap<CompanyCreateDto, Company>();
             CreateMap<Company, CompanyGetDto>();
+
             //Job
+            CreateMap<JobCreateDto, Job>();
+            CreateMap<Job, JobGetDto>()
+                .ForMember(dest => dest.CompanyName, opt => opt.MapFrom(src => src.Company.Name));
 
             //Candidate
         }
--- backend_server/backend_server/obj/Debug/net8.0/backend_server.AssemblyInfo.cs
+++ backend_server/backend_server/obj/Debug/net8.0/backend_server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a66d588db2a04511ca8247f4a9bd407872da663")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601392249359f7d0ef751c9ee96faa4ba3b72135")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
