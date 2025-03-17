399489adfcc093673c06489cf0e1466ad2e7ceaa
SanchayKanade <sanchay2598@gmail.com>
2025-03-17 13:04:44 -0500

    Homepage done dark mode not working

--- frontend_client/src/App.tsx
+++ frontend_client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, lazy, Suspense } from "react";
 import { ThemeContext } from "./context/theme.context";
 import Navbar from "./components/navbar/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import CustomLinearProgress from "./components/navbar/custom-linear-progress/CustomLinearProgress.component";
+
+// Imports with Lazy loading
+const Home = lazy(() => import("./pages/home/Home.page"));
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -11,7 +16,13 @@ const App = () => {
   return (
     <div className={appStyles}>
       <Navbar />
-      <div className="wrapper">Routes</div>
+      <div className="wrapper">
+        <Suspense fallback={<CustomLinearProgress />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 };
