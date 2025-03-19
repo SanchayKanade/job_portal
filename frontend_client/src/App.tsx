cef0188d65423b9b161d187b22eb6c38f1e71ca0
SanchayKanade <sanchay2598@gmail.com>
2025-03-19 13:29:02 -0500

    Candidates added

--- frontend_client/src/App.tsx
+++ frontend_client/src/App.tsx
@@ -11,6 +11,10 @@ const Companies = lazy(() => import("./pages/companies/Companies.page"));
 const AddCompany = lazy(() => import("./pages/companies/AddCompany.page"));
 const Jobs = lazy(() => import("./pages/jobs/Jobs.page"));
 const AddJob = lazy(() => import("./pages/jobs/AddJob.page"));
+const Candidates = lazy(() => import("./pages/candidates/Candidates.page"));
+const AddCandidates = lazy(
+  () => import("./pages/candidates/AddCandidate.page")
+);
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -31,6 +35,10 @@ const App = () => {
             <Route path="/jobs">
               <Route index element={<Jobs />} />
               <Route path="add" element={<AddJob />} />
+            </Route>
+            <Route path="/candidates">
+              <Route index element={<Candidates />} />
+              <Route path="add" element={<AddCandidates />} />
             </Route>
           </Routes>
         </Suspense>
--- frontend_client/src/types/global.typing.ts
+++ frontend_client/src/types/global.typing.ts
@@ -26,6 +26,26 @@ export interface IJob {
     level: string;
     companyId: string;
  }
+
+ export interface ICandidate {
+   id: string;
+   firstName: string;
+   lastName: string;
+   email: string;
+   phone: string;
+   coverLetter: string;
+   resumeUrl: string;
+   jobId: string;
+   jobTitle: string;
+}
+export interface ICreateCandidateDto {
+   firstName: string;
+   lastName: string;
+   email: string;
+   phone: string;
+   coverLetter: string;
+   jobId: string;
+}
 // [
 //     {
 //       "id": 1,
